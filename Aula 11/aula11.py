026bf2467357bb56d65635405528d4a722803859
Adonias Pessoa <82778067+Adonias1994@users.noreply.github.com>
2022-11-05 00:45:39 -0300

    Add files via upload
    Um pouco mais de prática da condicional if, elif e else.

---
+++ Aula 11/aula11.py
@@ -0,0 +1,13 @@
+a = 10
+b = 5
+res = 0
+op = ' / '
+if op == '+':
+    res = a + b
+elif op == '-':
+    res = a - b
+elif op == '*':
+    res = a * b
+elif op == '/':
+    res = a / b
+print('\n' + str(a) + op + str(b) + ' = ' + str(res) + '\n');
