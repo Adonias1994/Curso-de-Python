259da73990112da6d3dbfcd7cae33e454f936248
Adonias Pessoa <82778067+Adonias1994@users.noreply.github.com>
2022-12-04 13:17:48 -0300

    Delete Aula 12 directory

--- Aula 12/aula12.py
+++
@@ -1,9 +0,0 @@
-carros = ['HRV', 'Golf', 'Argo', 'Focus']
-for x in carros: # for pecorre todos elementos de carros na nova variável x
-    if(x == 'Argo'):
-        break
-    print(x)
-print('Fim do programa')
-print('\nPegadinha!\n')
-for y in carros:
-    print(y, '\n')
