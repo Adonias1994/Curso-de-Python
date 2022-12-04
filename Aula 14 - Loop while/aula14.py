7d22c29a051dd13aecca720ac5721db05cbead07
Adonias Pessoa <82778067+Adonias1994@users.noreply.github.com>
2022-12-04 13:20:05 -0300

    Add files via upload

---
+++ Aula 14 - Loop while/aula14.py
@@ -0,0 +1,14 @@
+import os
+os.system('cls')
+
+i = 0 # i é incremento
+
+print('\n')
+while i < 10:
+    i += 1
+    print(i)
+    if (i >= 5):
+        break # Interrompe o loop
+print('\n')
+
+#Continua...
---
+++ Aula 2 - Sintaxe Básica e Comentários/aula02.py
@@ -0,0 +1,6 @@
+#utf-8
+num1 = num2 = res=0
+canal = 'CFB Cursos' #Variável e string simples
+curso = 'Curso de Python'
+print(canal)
+print(curso)
