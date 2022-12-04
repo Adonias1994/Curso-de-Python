2833393d17aaa5b9f4679c2367f8609e721ebead
Adonias Pessoa <82778067+Adonias1994@users.noreply.github.com>
2022-12-04 13:18:11 -0300

    Delete Aula 3 directory

--- Aula 3/aula03.py
+++
@@ -1,7 +0,0 @@
-#utf-8
-var1 = 'Essa é uma variável global' #Variável global / Pode ser acessada de qualquer lugar do programa
-def cn():
-    var2 = 'Essa é uma variável local' #Variável local / Só pode ser chamada dentro do escopo
-    print(var1)
-    print(var2)
-cn()
