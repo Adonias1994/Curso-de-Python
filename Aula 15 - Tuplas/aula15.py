681806e3bafda1422e3aad91f4f1c160a6ff5f6c
Adonias Pessoa <82778067+Adonias1994@users.noreply.github.com>
2022-12-16 00:17:23 -0300

    Add files via upload

---
+++ Aula 15 - Tuplas/aula15.py
@@ -0,0 +1,8 @@
+import os
+os.system('cls')
+
+l_carros = ['HRV','Golf' ,'Argo' ] # l ---> Lista (É possível modificar os itens)
+t_carros = ('HRV','Golf' ,'Argo' ) # t ---> Tupla (Não é possível modificar os itens)
+
+for x in l_carros:
+        print(x)
