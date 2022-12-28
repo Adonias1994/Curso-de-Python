89c811334beade138614aeebaff115728fde2545
Adonias Pessoa <82778067+Adonias1994@users.noreply.github.com>
2022-12-28 13:28:24 -0300

    Update aula16.py
    Aula de matrizes

--- Aula 16 - Matrizes/aula16.py
+++ Aula 16 - Matrizes/aula16.py
@@ -5,9 +5,13 @@ os.system('cls')
 carros = [
     ['Modelo', 'HRV'],
     ['Fabricante', 'Honda'],
-    ['Ano', '2016']
+    ['Ano', 2016]
 ]
+carros[2][1] = 2022 # Exemplo de alteração de dados de determinado índice
+carros.append(['Cor', 'Branco']) # append adiciona elementos em nossa matriz
+carros[3][1] = 'Prata' # Alteração de dado em elemento que foi adicionado posteriormente
 
-print(carros[0][1])
-
-# CONTINUA...
+print('\nAbaixo segue alguns exemplos de matrizes:.\n')
+for l, c in carros: # l para linha e c para coluna
+    print('---> ' + l + ': ' + str(c) + '.')
+print('\nNão deixe de abrir o código e acompanhar com os comentários.\n')
