2d94d58aa4c1ff5475ee3eb4ac10761bc7aca159
Adonias Pessoa <82778067+Adonias1994@users.noreply.github.com>
2023-01-22 13:49:54 -0300

    Add files via upload

---
+++ Aula 17 - Dictionary/aula17.py
@@ -0,0 +1,13 @@
+import os
+os.system('cls')
+
+# Chave/Key -> Valor/Value
+carro = {
+    'Fabricante': 'Honda',
+    'Modelo': 'HRV',
+    'Ano': '2016',
+    'Cor': 'Prata'
+} # Exemplo de dicionário (coleção de dados sem ordem onde cada elemento possui um par chave/valor)
+
+print(carro) # Imprime todos os valores
+print(carro['Ano']) # Pode ser utilizado a chave para imprimir o conteúdo dela
